dd1fd565a6d4df0c39794f3babdd28505a7ae4d2
mdarif6 <md1arif015@gmail.com>
2023-03-21 23:54:33 +0530

    adding details page

--- src/pages/detailspage/DetailsPage.jsx
+++ src/pages/detailspage/DetailsPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { setPhotosData } from "../../features/albumSlice";
@@ -6,11 +6,22 @@ import "./DetailsPage.css";
 
 export default function DetailsPage() {
   const { albumID } = useParams();
-  console.log(albumID, "albID");
+
   const dispatch = useDispatch();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage, setRecordsPerPage] = useState(9);
   const photosData = useSelector((state) => state.albums.photosData);
   const albumData = useSelector((state) => state.albums.albumsData);
 
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = photosData.slice(
+    indexOfFirstRecord,
+    indexOfLastRecord
+  );
+  const numberOfPages = Math.ceil(photosData.length / recordsPerPage);
+  console.log(currentRecords, numberOfPages, "sdsldf");
   const albumURL3 = `https://jsonplaceholder.typicode.com/albums/${albumID}/photos`;
   function getPhotosData() {
     fetch(albumURL3)
@@ -24,7 +35,11 @@ export default function DetailsPage() {
 
   return (
     <div>
-      <h1>arif</h1>
+      <h1>
+        {photosData.map((item) => {
+          return <img src={item.thumbnailUrl} alt="thumb" />;
+        })}
+      </h1>
     </div>
   );
 }
